08ad61fcce0e26345f6904817741a53b53d11767
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2023-06-08 01:12:59 +0700

    feat(component): cards

--- components/molecules/Card.tsx
+++ components/molecules/Card.tsx
@@ -17,10 +17,36 @@ export default function Card(props: CardProps) {
   );
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  max-width: 354px;
+  text-align: center;
+`;
 
-const Number = styled.p``;
+const Number = styled.p`
+  font-size: 24px;
+  font-weight: 600;
+  margin-bottom: 56px;
+  padding-top: 12px;
+  width: 56px;
+  height: 56px;
+  border: 1px solid #584d62;
+  border-radius: 50%;
+  display: inline-block;
+`;
 
-const Title = styled.h3``;
+const Title = styled.h3`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 40px;
+  color: #24053e;
+  margin-bottom: 28px;
+`;
 
-const Desc = styled.p``;
+const Desc = styled.p`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 32px;
+  color: #24053e;
+`;
