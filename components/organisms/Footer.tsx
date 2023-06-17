52f8975f9131d5e199dcfbf962c2eec60713b4bf
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2023-06-17 22:47:43 +0700

    feat(component): add social footer

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 import Logo from "../atoms/Logo";
+import Social from "../atoms/Social";
 
 export default function Footer() {
   return (
     <Wrapper>
       <Logo source="logo-dark" alt="Logo" />
+      <Socials>
+        <Social link="/" src="facebook" />
+        <Social link="/" src="twitter" />
+        <Social link="/" src="instagram" />
+      </Socials>
     </Wrapper>
   );
 }
@@ -13,4 +19,11 @@ const Wrapper = styled.div`
   width: 100vw;
   padding: 60px 0;
   text-align: center;
+`;
+
+const Socials = styled.div`
+  display: flex;
+  gap: 30px;
+  justify-content: center;
+  margin-top: 57px;
 `;
